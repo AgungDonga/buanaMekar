a8391eb5636fceda9cbfbea94ccb00c1c2abd381
AgungDonga <agungldcorp@gmail.com>
2020-07-26 22:09:48 +0700

    Create User Account

--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/mainController.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/mainController.java
@@ -5,8 +5,17 @@
  */
 package com.example.buanaMekar.controllers;
 
+import com.example.buanaMekar.entities.User;
+import com.example.buanaMekar.services.UserService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -15,14 +24,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class mainController {
     
-    @RequestMapping("/")
-    public String viewHomePage(){
-        return "welcome";
-    }
+    @Autowired
+    private UserService service;
+    
+//    @RequestMapping("/")
+//    public String viewHomePage(){
+//        return "welcome";
+//    }
     
     @RequestMapping("/createuser")
     public String createUser(){
         return "createUser";
     }
     
+
+    @RequestMapping("/")
+    public String viewHomePage(Model model){
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers",listUsers);
+        return "welcome";
+
+    }
+
+    @RequestMapping("/new")
+    public String showNewUserForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "createUser";
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String saveUser(@ModelAttribute("user")User user){
+        service.save(user);
+        return "redirect:/";
+    }
+    
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditUserForm(@PathVariable(name = "id")Long id){
+        ModelAndView mav = new ModelAndView("edit_user");
+        User user = service.get(id);
+        mav.addObject("user",user);
+        return mav;
+    }
+    
+    @RequestMapping("/delete/{id}")
+    public String deleteUser(@PathVariable(name = "id")Long id){
+        service.delete(id);
+        
+        return "redirect:/";
+    }
 }
--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/repositories/UserRepository.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/repositories/UserRepository.java
@@ -5,15 +5,20 @@
  */
 package com.example.buanaMekar.repositories;
 import com.example.buanaMekar.entities.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Insane
  */
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>{
+    
+    List<User> findAll();
     
     @Query("SELECT u FROM User u WHERE u.username = :username")
     public User getUserByUsername(@Param("username")String username);
