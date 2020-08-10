a949ddcbdc6f1d0775400833d827fff3351f0c34
Bagus WIdyantoro Putro <baguswidyantoroputro@gmail.com>
2020-08-10 22:37:09 +0700

    update modal toko
    belum hapus css lama

--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/tokoController.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/tokoController.java
@@ -9,8 +9,10 @@ import com.example.buanaMekar.entities.Toko;
 import com.example.buanaMekar.services.TokoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +40,15 @@ public class tokoController {
         List<Toko> listTokos = service.listAll();
         model.addAttribute("listTokos",listTokos);
         return "listToko";
-
+    }
+    
+    @RequestMapping(value= "find/{id}", method = RequestMethod.GET, produces =  MimeTypeUtils.APPLICATION_JSON_VALUE)
+    public String find(@PathVariable("id") int id){
+        try {
+            
+        } catch (Exception e) {
+        }
+        return "a";
     }
     
     @RequestMapping("/toko/new")
