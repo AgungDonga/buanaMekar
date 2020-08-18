2a06ea4100a1bb11a0cee5dbc190881bfe19d291
Bagus WIdyantoro Putro <baguswidyantoroputro@gmail.com>
2020-08-18 23:23:22 +0700

    Perbaikan suratJalan Detail

--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/repositories/SuratJalanRepository.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/repositories/SuratJalanRepository.java
@@ -20,11 +20,11 @@ public interface SuratJalanRepository extends CrudRepository<SuratJalan, Integer
     
     List<SuratJalan> findAll();
     
-    @Query("SELECT sj, o FROM SuratJalan sj, Orderan o WHERE sj.status = 0 AND o.status = 1 Group by o.toko, sj.tglKirim")
+    @Query("SELECT sj, o FROM SuratJalan sj, Orderan o WHERE sj.status = 0 AND o.status = 1 AND o.id = sj.orderan Group by o.toko.namaToko, sj.tglKirim")
     List<SuratJalan> findAllSuratJalan();
     
     //@Query(value = "SELECT sj FROM SuratJalan WHERE id LIKE ?1 OR topic LIKE ?1 OR forum_description LIKE ?1 OR date_forum LIKE ?1")
     
-    @Query("SELECT sj, o FROM SuratJalan sj, Orderan o WHERE sj.status = 0 AND sj.tglKirim= ?2 AND o.toko.namaToko = ?1 ")
+    @Query("SELECT sj, o FROM SuratJalan sj, Orderan o WHERE sj.status = 0 AND sj.tglKirim= ?2 AND o.toko.namaToko = ?1 AND o.id = sj.orderan")
     List<SuratJalan> listDetailSuratJalan(String key1, String key2);
 }
