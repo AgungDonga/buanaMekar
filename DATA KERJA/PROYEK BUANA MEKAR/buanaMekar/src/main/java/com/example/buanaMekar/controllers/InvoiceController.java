a41694860b9a98f2e542d9e3c147a0701f4dc979
Bagus WIdyantoro Putro <baguswidyantoroputro@gmail.com>
2020-08-21 22:05:27 +0700

    ASEM KUI

--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/InvoiceController.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/InvoiceController.java
@@ -9,14 +9,15 @@ import com.example.buanaMekar.entities.Invoice;
 import com.example.buanaMekar.entities.SuratJalan;
 import com.example.buanaMekar.services.InvoiceService;
 import com.example.buanaMekar.services.SuratJalanService;
-import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -60,27 +61,45 @@ public class InvoiceController {
     public String saveInvoice(HttpServletRequest request){
         Invoice invoicenya = new Invoice();
         String arrayBulan[]={"O","I","II","III", "IV", "V","VI","VII","VIII","IX","X", "XI","XII"};
-        Double totalHarga= Double.MAX_VALUE;
+        Double totalHarga= 0.0;
         List<SuratJalan> listSuratJalans = serviceSuratJalan.listDetailSuratJalan(request.getParameter("id").replaceAll("%20", " "), request.getParameter("id2").replaceAll("%20", " "));
         for (int i = 0; i < listSuratJalans.size(); i++) {
-            totalHarga = totalHarga + Integer.valueOf(listSuratJalans.get(i).getOrderan().getTotalHarga());
+            totalHarga = totalHarga + Double.valueOf(listSuratJalans.get(i).getOrderan().getTotalHarga());
         }
+        
+        Double ppnnya = totalHarga * 10 / 100;
         for (int i = 0; i < listSuratJalans.size(); i++) {
-            invoicenya.setInvoice("/INV/BPK"+arrayBulan[Integer.valueOf(listSuratJalans.get(i).getTglKirim().substring(3, 4).replaceAll("0", ""))]+2020);
-            System.out.println("Bulan ke "+Integer.valueOf(listSuratJalans.get(i).getTglKirim().substring(3, 4).replaceAll("0", "")) +"- "+ listSuratJalans.get(i).getTglKirim().substring(3, 4));
+//            invoicenya.setInvoice("/INV/BPK"+arrayBulan[Integer.valueOf(listSuratJalans.get(i).getTglKirim().substring(3, 4).replaceAll("0", ""))]+"2020");
+//            System.out.println("Bulan ke "+Integer.valueOf(listSuratJalans.get(i).getTglKirim().substring(3, 4).replaceAll("0", "")));
             //invoice.setPpn(Integer.tlistSuratJalans.get(i).getOrderan().getTotalHarga());
+            invoicenya.setId(i);
             invoicenya.setStatus(0);
-            invoicenya.setSuratJalan(listSuratJalans.get(i));
+            SuratJalan sj = new SuratJalan();
+            sj.setId(listSuratJalans.get(i).getId());
+            invoicenya.setSuratJalan(sj);
+            System.out.println("a="+listSuratJalans.get(i).getId());
+//          
+            Calendar cal3 = Calendar.getInstance();
+            Date tgl;
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            System.out.println("tanggal = "+listSuratJalans.get(i).getTglKirim());
+            System.out.println(listSuratJalans.get(i).getTglKirim().substring(3, 5));
+//            invoicenya.setInvoice("/INV/BPK"+arrayBulan[Integer.parseInt(listSuratJalans.get(i).getTglKirim().substring(3, 4))]+"2020");
+            try {
+                tgl = sdf.parse(listSuratJalans.get(i).getTglKirim());
+                cal3.setTime(tgl);
+                cal3.add(Calendar.MONTH, 1);
+                System.out.println("3 Hari Setelah 30 hari adalah Tanggal : " + sdf.format(cal3.getTime()));
+                invoicenya.setTglJatuhTempo(sdf.format(cal3.getTime()));
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             
-            Calendar calTambah = Calendar.getInstance();
-
-            calTambah.setTime(Date.valueOf(listSuratJalans.get(i).getTglKirim()));
-            calTambah.add(Calendar.DAY_OF_MONTH, 30);
-            invoicenya.setTglJatuhTempo(calTambah.getTime().toString());
-            Double ppn =totalHarga * 0.1;
-            invoicenya.setPpn(ppn.toString());
+            Double result = totalHarga + ppnnya;
+            System.out.println(result);
+            invoicenya.setPpn(result.toString());
             service.save(invoicenya);
-            
         }
         
         return "redirect:/invoice";
