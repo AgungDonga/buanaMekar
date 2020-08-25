61805b9fe4371a4973c6fe0f880429f55f4b17e5
AgungDonga <agungldcorp@gmail.com>
2020-08-25 21:54:38 +0700

    yiha

--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/suratJalanController.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/controllers/suratJalanController.java
@@ -7,11 +7,14 @@ package com.example.buanaMekar.controllers;
 
 import com.example.buanaMekar.entities.SuratJalan;
 import com.example.buanaMekar.services.SuratJalanService;
+import java.io.FileNotFoundException;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,6 +26,11 @@ public class suratJalanController {
 
     @Autowired
     SuratJalanService service;
+    
+    @RequestMapping("/suratJalan/report/{format}")
+    public String generateReport(@PathVariable String format)throws FileNotFoundException, JRException{
+        return service.exportReport(format);
+    }
 
     @RequestMapping("/suratJalan")
     public String viewSuratJalanPage(Model model) {
--- DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/services/SuratJalanService.java
+++ DATA KERJA/PROYEK BUANA MEKAR/buanaMekar/src/main/java/com/example/buanaMekar/services/SuratJalanService.java
@@ -4,13 +4,27 @@
  * and open the template in the editor.
  */
 package com.example.buanaMekar.services;
+
 import com.example.buanaMekar.entities.Orderan;
 import com.example.buanaMekar.entities.SuratJalan;
 import com.example.buanaMekar.repositories.OrderanRepository;
 import com.example.buanaMekar.repositories.SuratJalanRepository;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 /**
  *
@@ -18,22 +32,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SuratJalanService {
-    
+
     @Autowired
     SuratJalanRepository repo;
-    
+
     @Autowired
     OrderanRepository orderanRepo;
-    
+
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        String path  = "C:\\Users\\Insane\\Desktop\\Report";
+        List<SuratJalan> suratJalans = repo.findAll();
+
+        File file = ResourceUtils.getFile("classpath:suratJalan.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(suratJalans);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("CreatedBy", "Developer");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\suratJalan.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\suratJalan.pdf");
+        }
+        return "success : " + path;
+    }
+
     public List<SuratJalan> listAll() {
         return repo.findAll();
     }
-    
+
     public List<SuratJalan> listAllSuratJalan() {
         return repo.findAllSuratJalan();
     }
-    
-    public List<SuratJalan> listDetailSuratJalan(String key1, String key2){
+
+    public List<SuratJalan> listDetailSuratJalan(String key1, String key2) {
         return repo.listDetailSuratJalan(key1, key2);
     }
 
@@ -51,9 +84,8 @@ public class SuratJalanService {
         repo.deleteById(integer);
 
     }
-    
+
 //    public List<Orderan> getAllOrderan() {
 //        return orderanRepo.findAll();
 //    }
-    
 }
